f72cc3863209e3627904d3046647e4091e98d3ad
IVAN PINZA <ivandajopinza@gmail.com>
2023-04-22 20:45:27 -0500

    finalfinal

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = ({setIsShowForm}) => {
     setIsShowForm((isShowForm)=> !isShowForm)
   }
   return (
-   <header className=' md:flex relative max-w-3xl place-content-center'>
+   <header className='md:flex relative max-w-5xl place-content-center'>
     <h1 className='text-[30px] font-bold m-10'>Usuarios</h1>
     <button onClick={handleClickShowModal} className='bg-purple-p text-white p-2 hover:bg-purple-p/90 transition-colors text-sm absolute right-4 top-1/2 translate-y-[-50%]'>+ Crear nuevo usuario</button>
    </header>
